008ebc2ba497e44690a0f4957a9b7478443585f2
Nguyên Trần <nguyentranthepog@gmail.com>
2021-06-29 11:11:32 +0700

    feat: add Navbar component

--- src/components/Navbar/NavMenu.tsx
+++ src/components/Navbar/NavMenu.tsx
@@ -1,33 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { palette } from '../../styles/GlobalStyle';
-import { rgba } from 'polished';
 import NavMenuItem from './NavMenuItem';
 import { Button, ButtonColor, ButtonVariant } from '../Button';
 
-const StyledNavMenu = styled.nav`
-  position: fixed;
-  top: 0;
-  right: 0;
-  width: 280px;
-  height: 100vh;
-  padding-top: 84px;
-  background-color: ${palette.neutral.white};
-  z-index: 20;
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: ${rgba(`${palette.neutral.black}`, 0.45)};
-  z-index: 10;
-`;
-
-const NavMenuList = styled.ul``;
-
 const NavMenuAuth = styled.div`
+  position: relative;
   padding: 0 16px;
   margin-top: 8px;
 
@@ -36,32 +13,73 @@ const NavMenuAuth = styled.div`
     display: block;
   }
 
-  & button:first-child {
+  & button:first-of-type {
     margin: 24px 0;
+  }
+
+  & div {
+    width: 100%;
+    height: 1px;
+    background-color: ${palette.neutral.gray};
   }
 `;
 
-const NavMenu = () => {
+const StyledNavMenu = styled.nav<{ isMenuOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 20;
+  width: 280px;
+  height: 100vh;
+  padding-top: 84px;
+  background-color: ${palette.neutral.white};
+  transform: translateX(100%);
+  transition: all 0.25s linear;
+
+  ${(props) =>
+    props.isMenuOpen &&
+    css`
+      transform: translateX(0%);
+    `}
+
+  @media screen and (min-width: 1024px) {
+    position: static;
+    width: auto;
+    height: auto;
+    padding: 0;
+    background-color: transparent;
+    transform: translateX(0%);
+
+    & ul {
+      display: flex;
+      align-items: center;
+    }
+
+    & ${NavMenuAuth} {
+      display: none;
+    }
+  }
+`;
+
+const NavMenu = (props: { isMenuOpen: boolean }) => {
   return (
-    <>
-      <StyledNavMenu>
-        <NavMenuList>
-          <NavMenuItem href="/">About</NavMenuItem>
-          <NavMenuItem href="/">Service</NavMenuItem>
-          <NavMenuItem href="/">Projects</NavMenuItem>
-          <NavMenuItem href="/">Contacts</NavMenuItem>
-        </NavMenuList>
-        <NavMenuAuth>
-          <Button colorType={ButtonColor.Secondary} variant={ButtonVariant.Outlined}>
-            Login
-          </Button>
-          <Button colorType={ButtonColor.Secondary} variant={ButtonVariant.Filled}>
-            Sign Up
-          </Button>
-        </NavMenuAuth>
-      </StyledNavMenu>
-      <Overlay />
-    </>
+    <StyledNavMenu isMenuOpen={props.isMenuOpen}>
+      <ul>
+        <NavMenuItem href="/">About</NavMenuItem>
+        <NavMenuItem href="/">Service</NavMenuItem>
+        <NavMenuItem href="/">Projects</NavMenuItem>
+        <NavMenuItem href="/">Contacts</NavMenuItem>
+      </ul>
+      <NavMenuAuth>
+        <div></div>
+        <Button colorType={ButtonColor.Secondary} variant={ButtonVariant.Outlined}>
+          Login
+        </Button>
+        <Button colorType={ButtonColor.Secondary} variant={ButtonVariant.Filled}>
+          Sign Up
+        </Button>
+      </NavMenuAuth>
+    </StyledNavMenu>
   );
 };
 
--- src/components/Navbar/NavMenuItem.tsx
+++ src/components/Navbar/NavMenuItem.tsx
@@ -3,27 +3,46 @@ import styled from 'styled-components';
 import { Caption, TypoTag } from '../Typography';
 import { palette } from '../../styles/GlobalStyle';
 
-const StyledNavMenuItem = styled.li``;
+const StyledNavMenuItem = styled.li`
+  @media screen and (min-width: 1024px) {
+    & + & {
+      margin-left: 32px;
+    }
+  }
+`;
 
-const NavMenuLink = styled.a`
+const NavMenuLink = styled.a<{ isActive?: boolean }>`
   width: 100%;
   padding: 15px 32px;
   color: ${palette.neutral.gray};
   transition: all 0.15s linear;
 
-  &:hover {
-    color: ${palette.neutral.white};
-    background-color: ${palette.secondary.light};
+  @media screen and (max-width: 991px) {
+    &:hover {
+      color: ${palette.neutral.white};
+      background-color: ${palette.secondary.light};
+    }
+    &:active {
+      background-color: ${palette.secondary.dark};
+    }
   }
-  &:active {
-    background-color: ${palette.secondary.dark};
+
+  @media screen and (min-width: 1024px) {
+    padding: 0;
+    color: ${palette.neutral.black};
+
+    &:hover {
+      color: ${palette.neutral.gray};
+    }
+
+    ${(props) => props.isActive && `color: ${palette.neutral.gray}`}
   }
 `;
 
-const NavMenuItem: React.FC<{ href: string }> = (props) => {
+const NavMenuItem: React.FC<{ href: string; isActive?: boolean }> = (props) => {
   return (
     <StyledNavMenuItem>
-      <NavMenuLink href={props.href}>
+      <NavMenuLink href={props.href} isActive={props.isActive}>
         <Caption typoTag={TypoTag.Span}>{props.children}</Caption>
       </NavMenuLink>
     </StyledNavMenuItem>
