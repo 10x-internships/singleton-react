9ad34cb8a97889a00658fd6c9092438c5a414269
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-01 16:47:42 +0700

    fix: fix gutters and add more props

--- src/components/Row.ts
+++ src/components/Row.ts
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 
+export enum Align {
+  DEFAULT = 'stretch',
+  START = 'flex-start',
+  END = 'flex-end',
+  CENTER = 'center',
+}
+export enum JustifyContent {
+  DEFAULT = 'flex-start',
+  BETWEEN = 'space-between',
+}
+
 type RowProps = {
   noWrap?: boolean;
   rowGap?: number;
+  align?: Align;
+  justifyContent?: JustifyContent;
 };
 
 const Row = styled.div<RowProps>`
   display: flex;
   flex-wrap: ${(props) => (props.noWrap ? 'nowrap' : 'wrap')};
-  justify-content: space-between;
-  column-gap: 20px;
+  align-items: ${(props) => (props.align ? props.align : Align.DEFAULT)};
+  justify-content: ${(props) => (props.justifyContent ? props.justifyContent : JustifyContent.DEFAULT)};
+  margin-left: -20px;
   ${(props) => props.rowGap && `row-gap: ${props.rowGap}px;`};
 
   @media screen and (min-width: 1200px) {
-    column-gap: 30px;
+    margin-left: -30px;
   }
 `;
 
