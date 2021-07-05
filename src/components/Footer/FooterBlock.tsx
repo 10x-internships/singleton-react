435d59d873804296014e790e78d256ffa3e5d6e1
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-05 15:46:39 +0700

    fix: add hover state

--- src/components/Footer/FooterBlock.tsx
+++ src/components/Footer/FooterBlock.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { palette } from '../../styles/GlobalStyle';
-import { Caption, Body, TypoTag } from '../Typography';
+import { Caption, Body, CaptionWrapper, TypoTag } from '../Typography';
 
 type FooterLinkArray = {
   name: string;
@@ -13,7 +13,7 @@ type FooterBlockProp = {
 };
 
 const StyledFooterBlock = styled.div`
-  & h4 {
+  & ${CaptionWrapper} {
     margin-bottom: 14px;
     color: ${palette.neutral.white};
   }
@@ -22,16 +22,21 @@ const StyledFooterBlock = styled.div`
 const FooterLink = styled.a`
   color: ${palette.accent.blueLight};
   display: block;
+  transition: all 0.25s linear;
 
   & + & {
     margin-top: 4px;
+  }
+
+  &:hover {
+    color: ${palette.neutral.white};
   }
 `;
 
 const FooterBlock = (props: FooterBlockProp) => {
   return (
     <StyledFooterBlock>
-      <Caption typoTag={TypoTag.H4}>{props.title}</Caption>
+      <Caption typoTag={TypoTag.H2}>{props.title}</Caption>
       {props.links.map((link, index) => (
         <FooterLink key={index} href={link.path}>
           <Body typoTag={TypoTag.Span}>{link.name}</Body>
